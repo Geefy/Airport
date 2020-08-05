179b1193a6b91325545ac6b2119febed9df27545
René Lorentzen <rene2756@zbc.dk>
2020-08-05 22:31:12 +0200

    Add project files.

---
+++ Airport/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Airport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new KøbenhavnsLufthavnEntities())
+            {
+                var planeRoute = new PlaneRoute()
+                {
+                    AirportOne = "CPH",
+                    AirportTwo = "LAX",
+                    RouteOwner = 1
+                };
+                context.PlaneRoutes.Add(planeRoute);
+
+                context.SaveChanges();
+            }
+        }
+    }
+}
